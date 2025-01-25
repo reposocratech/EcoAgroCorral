4c07dc5ca5a92ff7a5741f57b702cddb180a57ec
david <davidcf18@gmail.com>
2025-01-25 17:37:34 +0100

    Retoques de estilos

--- Client/src/pages/Experiences/AllExperiences/AllExperiences.jsx
+++ Client/src/pages/Experiences/AllExperiences/AllExperiences.jsx
@@ -4,6 +4,7 @@ import { fetchData } from "../../../helpers/axiosHelper.js";
 import { ExperiencePresentationCard } from "../../../components/ExperiencePresentationCard/ExperiencePresentationCard.jsx";
 import { useNavigate } from "react-router-dom";
 import { AgroContext } from "../../../context/ContextProvider.jsx";
+import "./AllExperiences.css"
 
 export const AllExperiences = () => {
   const { user } = useContext(AgroContext);
@@ -26,7 +27,7 @@ export const AllExperiences = () => {
   return (
     <>
       <section>
-        <Container fluid="xxl" className="pb-5">
+        <Container fluid="xxl" className="pb-3">
           {experiences.map((elem, index) => {
             return (
               <Row key={elem.experience_id} className="mt-3">
@@ -35,7 +36,7 @@ export const AllExperiences = () => {
             );
           })}
           {user?.user_type === 1 && (
-            <div className="mt-5 d-flex justify-content-center flex-wrap gap-3">
+            <div className="btnAllExperiences d-flex justify-content-center align-content-center align-items-center flex-wrap gap-3">
               <Button
                 className="button-nuevo-paseo"
                 onClick={() => navigate("/experiencias/createExperience")}
